78b4417761507e56d6143d42d921168b298283c5
lseong <lseong96@gamil.com>
2023-10-17 20:31:37 +0900

    list

--- src/main/java/kr/co/lottemarket/cs/mapper/ArticleMapper.java
+++ src/main/java/kr/co/lottemarket/cs/mapper/ArticleMapper.java
@@ -1,9 +1,14 @@
 package kr.co.lottemarket.cs.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.lottemarket.dto.ArticleDTO;
 
 @Mapper
 public interface ArticleMapper {
-
-	public void selectArticle(int no);
+	
+	public List<ArticleDTO> selectArticles(int group,int cate1);
+	
 }
