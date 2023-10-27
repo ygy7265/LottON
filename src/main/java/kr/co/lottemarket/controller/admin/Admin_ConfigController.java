902980c9b524344fb08bca7587bc8d0158e634da
bin <fg012023@gmail.com>
2023-10-27 16:16:55 +0900

    회원목록

--- src/main/java/kr/co/lottemarket/controller/admin/Admin_ConfigController.java
+++ src/main/java/kr/co/lottemarket/controller/admin/Admin_ConfigController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.transaction.Transactional;
+import kr.co.lottemarket.dto.user.TermsDTO;
 import kr.co.lottemarket.service.admin.AdminService;
 import lombok.extern.log4j.Log4j2;
 
@@ -24,7 +25,11 @@ public class Admin_ConfigController {
 	private AdminService adminService;
 	
 	@GetMapping("/admin/layout/config/info")
-	public String info() {
+	public String info(Model model) {
+		
+		List<TermsDTO> termslist = adminService.selectPolicy();
+		
+		model.addAttribute("termslist", termslist);
 		
 		return "/admin/layout/config/info";
 	}
