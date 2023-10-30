8f571119d1a0a1df81ad113de0a9a61e84786e9c
bin <fg012023@gmail.com>
2023-10-30 09:06:06 +0900

    수정

--- src/main/java/kr/co/lottemarket/controller/admin/Admin_CsController.java
+++ src/main/java/kr/co/lottemarket/controller/admin/Admin_CsController.java
@@ -380,7 +380,7 @@ public class Admin_CsController {
 		 // 페이지당 항목 수
 	    int itemsPerPage = pageSize;
 
-	    // 총 항목 수 (공지사항 목록의 크기)
+	    // 총 항목 수
 	    int totalItems = adminService.selectCountUser();
 
 	    // totalPages 계산
